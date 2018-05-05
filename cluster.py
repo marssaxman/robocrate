2a0a5c454eeddee4538494a099baafc6f8f2ef46
Mars Saxman <mars@redecho.org>
2018-05-04 18:38:29 -0700

    using gaussian mixture model for better hidden variable discovery

--- cluster.py
+++ cluster.py
@@ -6,9 +6,15 @@ import analysis
 import numpy as np
 import random
 import sklearn.cluster
+import sklearn.mixture
 import sklearn.preprocessing
+import sklearn.random_projection
 import library
 from collections import Counter
+
+# info here:
+# https://jakevdp.github.io/PythonDataScienceHandbook/05.12-gaussian-mixtures.html
+
 
 
 def _calc_feats(path):
@@ -87,19 +93,22 @@ def cluster():
     # Normalize the input data.
     feats = sklearn.preprocessing.scale(feats, axis=0)
 
-    # Build a classifier. We'll eventually do something clever like using the
-    # elbow method, but for now we'll assume there should be roughly 200 tracks
-    # in each cluster.
-    n_clusters = max(3, int(len(tracks) / 200.0))
-    model = sklearn.cluster.KMeans(n_clusters=n_clusters)
-    # Convert the list of feature vectors into a matrix and fit the model.
-    # Perhaps we should normalize? PCA might also be valuable.
+    # Reduce dimensionality.
+    transformer = sklearn.random_projection.SparseRandomProjection(eps=0.25)
+    feats = transformer.fit_transform(feats)
+
+    n_clusters = max(3, int(len(tracks) / 150.0))
+    model = sklearn.mixture.GaussianMixture(
+        n_components=n_clusters, covariance_type='full')
+
     print "fitting model"
     model.fit(feats)
 
     # Make predictions: which tracks should go into which clusters?
     print "generating predictions"
     labels = model.predict(feats)
+    proba = model.predict_proba(feats)
+
     # Sort the tracks into groups, corresponding to each generated label.
     groups = [list() for i in range(n_clusters)]
     for i, track in enumerate(tracks):
@@ -107,12 +116,24 @@ def cluster():
 
     # Print a report describing the crates we've found.
     for i, crate in enumerate(groups):
+        if len(crate) < 4:
+            continue
         print "Crate %d contains %d tracks" % (i, len(crate))
 
-        # Which are the most-representative tracks in this cluster?
-        distances = model.transform(feats)[:, i]
-        bestfits = np.argsort(distances)[::]
+        # Print out a playlist with all these tracks.
+        with open(os.path.join(os.getcwd(), "crate%d.m3u" % i), 'w') as fd:
+            for t in crate:
+                if not t.source is None:
+                    fd.write(t.source.encode('utf-8') + '\n')
 
+        # Which are the most-representative tracks in this cluster?
+        clustermean = model.means_[i,:]
+        #from sklearn.metrics.pairwise import euclidean_distances
+        #clusterdist = euclidean_distances(feats, [clustermean])[:,0]
+        from sklearn.metrics.pairwise import cosine_distances
+        clusterdist = cosine_distances(feats, [clustermean])[:,0]
+
+        bestfits = np.argsort(clusterdist)
         print "  Representative tracks:"
         for j in bestfits[:5]:
             info = tracks[j]
@@ -121,16 +142,15 @@ def cluster():
         # Which are the most frequently represented artists?
         print "  Most frequent artists:"
         artists = Counter(t.artist for t in crate if not t.artist is None)
-        print "    %s" % ", ".join(k for k,v in artists.most_common(3))
+        print "    %s" % ", ".join(k for k,v in artists.most_common(12))
         print "  Most common genres:"
         print "    %s" % ", ".join(_top3desc(t.genre for t in crate))
 
         # What is a representative BPM range?
-        bpms = [t.bpm for t in crate if not t.bpm is None]
-        bpms = np.array(bpms, dtype=np.float)
-        bpm_mean, bpm_std = np.mean(bpms), np.std(bpms)
-        bpm_lo = int(round(bpm_mean - bpm_std))
-        bpm_hi = int(round(bpm_mean + bpm_std))
-        print "  Tempo range: %d-%d" % (bpm_lo, bpm_hi)
+        if len(crate) > 1:
+            bpms = [t.bpm for t in crate if not t.bpm is None]
+            bpms = np.array(bpms, dtype=np.float)
+            bpm_mean, bpm_std = np.mean(bpms), np.std(bpms)
+            print "  Tempo: %d bpm (+/- %d)" % (bpm_mean, bpm_std)
 
         print ""
