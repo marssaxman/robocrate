e3069ab722fef75f3d5e4fbea365ad6c7848878e
Mars Saxman <mars@redecho.org>
2018-05-10 08:14:25 -0700

    trying out an array of MFCC-only features

--- experiments/features.py
+++ experiments/features.py
@@ -2,6 +2,7 @@ import os
 import os.path
 import numpy as np
 from analysis import mfcc, spectral
+from scipy.fftpack import dct
 
 import matplotlib
 matplotlib.use('Agg')
@@ -59,12 +60,13 @@ def plot_features(track, feats_A, feats_B):
     plt.savefig(_caption(track)+'_feats.png', dpi=96, bbox_inches='tight')
 
 
+def hamming(N):
+    # improved hamming window: original implementation used 0.54, 0.46
+    i = np.arange(N).astype(np.float)
+    return 0.53836 - (0.46164 * np.cos(np.pi * 2.0 * i / (N-1)))
+
 
 def altfeats(clip):
-    def hamming(N):
-        # improved hamming window: original implementation used 0.54, 0.46
-        i = np.arange(N).astype(np.float)
-        return 0.53836 - (0.46164 * np.cos(np.pi * 2.0 * i / (N-1)))
 
     def zcr(frame):
         count = len(frame)
@@ -95,7 +97,85 @@ def altfeats(clip):
     return np.array(clipfeats)
 
 
+def hertz_to_mel(freq):
+    return 2595.0 * np.log10(1 + (freq/700.0))
+
+
+def mel_to_hertz(mel):
+    return 700.0 * (10**(mel/2595.0)) - 700.0
+
+
+def melfilter(num_mels, num_ffts, samplerate, freq_min, freq_max):
+    # compute the mel values for the specified frequency range
+    mel_max = hertz_to_mel(freq_max)
+    mel_min = hertz_to_mel(freq_min)
+    delta_mel = np.abs(mel_max - mel_min) / (num_mels + 1.0)
+    # generate triangular filter center, lower, and upper freqs for each band
+    freqs = mel_to_hertz(mel_min + delta_mel * np.arange(0, num_mels+2))
+    edges = zip(freqs[1:-1], freqs[:-2], freqs[2:])
+    # populate a filterbank matrix
+    fbank = np.zeros((num_mels, num_ffts))
+    fft_freqs = np.linspace(0.0, samplerate/2.0, num_ffts)
+    for mel_band, (center, lower, upper) in enumerate(edges):
+        # the rising slope includes all bins from the lower edge to the center
+        rising = (fft_freqs >= lower) == (fft_freqs <= center)
+        fbank[mel_band, rising] = (
+            (fft_freqs[rising] - lower) / (center - lower)
+        )
+        # the falling slope includes FFT bins from the center to the upper edge
+        falling = (fft_freqs >= center) == (fft_freqs <= upper)
+        fbank[mel_band, falling] = (
+            (upper - fft_freqs[falling]) / (upper - center)
+        )
+    return fbank
+
+
+mel_fbank = melfilter(24, 513, 22050.0, 64.0, 11025.0)
+
+
+def mfcc(clip):
+    # assume clip is 22050Hz mono; normalize it
+    clip = clip.astype(np.float)
+    clip -= clip.mean()
+    clip /= np.abs(clip).max()
+    # we'll take 1024-sample FFTs every 512 samples
+    frame_len = 1024
+    step_len = 512
+    window = hamming(frame_len)
+    num_frames = int(np.floor((len(clip) - frame_len + 1) / step_len))
+    num_ceps = 13
+    clip_ceps = np.zeros((num_frames, num_ceps))
+    for i in xrange(num_frames):
+        offset = i * step_len
+        # get the samples and multiply by the window to reduce ringing
+        frame = clip[offset:offset+frame_len] * window
+        # get the magnitude spectrum
+        s = np.abs(np.fft.rfft(frame))
+        # compute power
+        s = np.square(s) / float(frame_len)
+        # apply mel-scaled filter to get mel-band energies for this frame
+        mels = np.dot(s, mel_fbank.T)
+        # take log to account for perceptual nonlinearity
+        mels = np.log(mels + np.finfo(np.float).eps)
+        # take DCT to convert into cepstrum domain
+        frame_ceps = dct(mels, type=2, norm='ortho')
+        # should we 'lifter' the ceps here?
+        clip_ceps[i,:] = frame_ceps[:num_ceps]
+    return clip_ceps
+
+
+def mfccfeats(clip):
+    # Compute the MFCC series for this clip.
+    # For each coefficient, return the mean and standard deviation, plus the
+    # mean difference between adjacent coefficients.
+    ceps = mfcc(clip)
+    diffs = np.abs(ceps[:-1,:] - ceps[1:,:])
+    feats = (ceps.mean(axis=0), ceps.std(axis=0), diffs.mean(axis=0))
+    return np.concatenate(feats)
+
+
 def plot_feats(feats, ax):
+    # Display distributions of features with a violin plot.
     pos = np.arange(feats.shape[1])
     ax.violinplot(feats, pos, points=400, vert=False, widths=0.9,
             showmeans=False, showextrema=True, showmedians=True)
@@ -108,25 +188,30 @@ def plot_feats(feats, ax):
 def run(clips):
     # Measure the effectiveness of different features for music similarity
     # calculation.
-    # Compute the distribution of feature values across the track list.
-    # Plot a histogram for each feature.
+    feats_A = list()
+    feats_B = list()
+    for t, clip_A, _, clip_B, _ in clips:
+        feats_A.append(mfccfeats(clip_A))
+        feats_B.append(mfccfeats(clip_B))
+    feats_A = np.array(feats_A)
+    feats_B = np.array(feats_B)
 
-    orig_feats = list()
-    for t, clip_A, feats_A, clip_B, feats_B in clips:
-        orig_feats.append(feats_A)
-        orig_feats.append(feats_B)
-    orig_feats = np.concatenate(orig_feats, axis=0)
+    # normalize
+    summed = np.vstack((feats_A, feats_B))
+    summedmean = summed.mean(axis=0)
+    summedstd = summed.std(axis=0)
+    feats_A = (feats_A - summedmean) / summedstd
+    feats_B = (feats_B - summedmean) / summedstd
 
-    norm_feats = (orig_feats - orig_feats.mean(axis=0)) / orig_feats.std(axis=0)
-
-    # Display distributions of features with a violin plot.
+    # plot similarity across corresponding track pairs
+    plt.set_cmap('hot')
     fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12, 8))
-
-    plot_feats(orig_feats, axes[0])
-    axes[0].set_title("tracks db features")
-
-    plot_feats(norm_feats, axes[1])
-    axes[1].set_title("normalized features")
-
+    plt.set_cmap('hot')
+    axes[0].matshow(np.array(feats_A), vmin=-3, vmax=3)
+    axes[1].matshow(np.array(feats_B), vmin=-3, vmax=3)
+    #plot_feats(feats_A, axes[0])
+    #axes[0].set_title("tracks db features")
+    #plot_feats(feats_B, axes[1])
+    #axes[1].set_title("normalized features")
     plt.savefig("features.png", dpi=96, bbox_inches='tight')
 
