4397944bee4a560db434b542594ea27042fe3944
Mars Saxman <mars@redecho.org>
2018-05-10 10:51:53 -0700

    seems like a combination of tempogram and mfcc statistics might work

--- experiments/features.py
+++ experiments/features.py
@@ -97,6 +97,25 @@ def altfeats(clip):
     return np.array(clipfeats)
 
 
+def stft(clip, frame_len=1024):
+    # generator for magnitude spectrum frames from an audio clip
+    # assume clip is 22050Hz mono; normalize it
+    clip = clip.astype(np.float)
+    clip -= clip.mean()
+    clip /= np.abs(clip).max()
+    # assume half-overlap
+    step_len = int(frame_len / 2)
+    window = hamming(frame_len)
+    num_frames = int(np.floor((len(clip) - frame_len + 1) / step_len))
+    for i in xrange(num_frames):
+        offset = i * step_len
+        # get the samples and multiply by the window to reduce ringing
+        frame = clip[offset:offset+frame_len] * window
+        # get the magnitude spectrum
+        s = np.abs(np.fft.rfft(frame))
+        yield s
+
+
 def hertz_to_mel(freq):
     return 2595.0 * np.log10(1 + (freq/700.0))
 
@@ -160,7 +179,8 @@ def mfcc(clip):
         # take DCT to convert into cepstrum domain
         frame_ceps = dct(mels, type=2, norm='ortho')
         # should we 'lifter' the ceps here?
-        clip_ceps[i,:] = frame_ceps[:num_ceps]
+        # in any case, skip c0 (total energy) and the higher-order ceps
+        clip_ceps[i,:] = frame_ceps[1:1+num_ceps]
     return clip_ceps
 
 
@@ -171,6 +191,90 @@ def mfccfeats(clip):
     ceps = mfcc(clip)
     diffs = np.abs(ceps[:-1,:] - ceps[1:,:])
     feats = (ceps.mean(axis=0), ceps.std(axis=0), diffs.mean(axis=0))
+    return np.concatenate(feats)
+
+
+def tempogram(novelty):
+    # compute a tempogram over the novelty function, using 256 steps of 512
+    # samples each, which is roughly six seconds; enough for tempo information
+    # and a sense of the general rhythm pattern.
+    step_len = 256
+    num_frames = int(np.floor((len(novelty) - step_len + 1) / step_len))
+    tempogram = np.zeros(step_len, dtype=np.float)
+    for i in xrange(num_frames):
+        offset = i * step_len
+        frame = novelty[offset:offset+step_len]
+        corr = np.correlate(frame, frame, mode='full')
+        corr = corr[corr.size / 2:]
+        tempogram += corr / np.max(np.abs(corr))
+    # Since we've been summing each tempogram as we go, we can get the average
+    # now by dividing over the number of frames.
+    return tempogram / float(num_frames)
+
+
+def rhythmfeats(clip):
+    # Compute an activation tempogram for this clip.
+    # assume clip is 22050Hz mono; normalize it
+    clip = clip.astype(np.float)
+    clip -= clip.mean()
+    clip /= np.abs(clip).max()
+    # we'll take 1024-sample FFTs every 512 samples
+    frame_len = 1024
+    step_len = 512
+    window = hamming(frame_len)
+    num_frames = int(np.floor((len(clip) - frame_len + 1) / step_len))
+    prev = None
+    novelty = np.zeros(num_frames, dtype=np.float)
+    for i in xrange(num_frames):
+        offset = i * step_len
+        # get the samples and multiply by the window to reduce ringing
+        frame = clip[offset:offset+frame_len] * window
+        # get the magnitude spectrum
+        s = np.abs(np.fft.rfft(frame))
+        # convert to decibels for perceptual scaling
+        s = 20. * np.log10(s + np.finfo(np.float).eps)
+        if not prev is None:
+            # median positive flux is the novelty value
+            novelty[i] = np.median(np.maximum(s - prev, 0))
+        prev = s
+    # compute a tempogram over the novelty function. We'll skip the first few
+    # steps, since they come too close together to perceive distinct events,
+    # then return the following 32 values, which is enough to see the basic
+    # rhythm pattern over a handful of beats.
+    return tempogram(novelty)[4:36]
+
+
+def combofeats(clip):
+    # Compute a feature vector including both rhythm and timbre features.
+    frame_len = 1024
+    prev_db = None
+    num_ceps = 13
+    clip_ceps = list()
+    novelty = list()
+    for s in stft(clip, frame_len):
+        # Compute MFCC for this frame.
+        s_power = np.square(s) / float(frame_len)
+        # apply mel-scaled filter to get mel-band energies for this frame
+        mels = np.dot(s_power, mel_fbank.T)
+        # take log to account for perceptual nonlinearity
+        mels = np.log(mels + np.finfo(np.float).eps)
+        # take DCT to convert into cepstrum domain
+        frame_ceps = dct(mels, type=2, norm='ortho')
+        # should we 'lifter' the ceps here?
+        # in any case, skip c0 (total energy) and the higher-order ceps
+        clip_ceps.append(frame_ceps[1:1+num_ceps])
+        # Compute onset superflux.
+        # convert to decibels for perceptual scaling
+        s_db = 20. * np.log10(s + np.finfo(np.float).eps)
+        if not prev_db is None:
+            # median positive flux is the novelty value
+            novelty.append(np.median(np.maximum(s_db - prev_db, 0)))
+        prev_db = s_db
+
+    rhythm = tempogram(np.array(novelty))[4:36]
+    ceps = np.array(clip_ceps)
+    cepdiff = np.abs(ceps[:-1,:] - ceps[1:,:])
+    feats = (rhythm, ceps.mean(axis=0), ceps.std(axis=0), cepdiff.mean(axis=0))
     return np.concatenate(feats)
 
 
@@ -185,14 +289,18 @@ def plot_feats(feats, ax):
     ax.axhspan(21-0.2,32+0.2, facecolor='blue', alpha=0.15)
 
 
+from sklearn.decomposition import PCA
+import sklearn.preprocessing
+
+
 def run(clips):
     # Measure the effectiveness of different features for music similarity
     # calculation.
     feats_A = list()
     feats_B = list()
     for t, clip_A, _, clip_B, _ in clips:
-        feats_A.append(mfccfeats(clip_A))
-        feats_B.append(mfccfeats(clip_B))
+        feats_A.append(combofeats(clip_A))
+        feats_B.append(combofeats(clip_B))
     feats_A = np.array(feats_A)
     feats_B = np.array(feats_B)
 
@@ -200,18 +308,32 @@ def run(clips):
     summed = np.vstack((feats_A, feats_B))
     summedmean = summed.mean(axis=0)
     summedstd = summed.std(axis=0)
+    summed = (summed - summedmean) / summedstd
     feats_A = (feats_A - summedmean) / summedstd
     feats_B = (feats_B - summedmean) / summedstd
 
+    # reduce dimensionality
+    pca = PCA(n_components=32)
+    pca.fit(summed)
+    reduced_A = pca.transform(feats_A)
+    reduced_B = pca.transform(feats_B)
+    captured = pca.explained_variance_ratio_.sum() * 100
+    print "PCA captured %.2f%% of the variance in the library" % captured
+
+
     # plot similarity across corresponding track pairs
     plt.set_cmap('hot')
-    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12, 8))
+    fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(12, 12))
     plt.set_cmap('hot')
-    axes[0].matshow(np.array(feats_A), vmin=-3, vmax=3)
-    axes[1].matshow(np.array(feats_B), vmin=-3, vmax=3)
-    #plot_feats(feats_A, axes[0])
-    #axes[0].set_title("tracks db features")
-    #plot_feats(feats_B, axes[1])
-    #axes[1].set_title("normalized features")
+    axes[0,0].matshow(np.array(feats_A))
+    axes[0,0].set_title("feats_A")
+    axes[0,1].matshow(np.array(feats_B))
+    axes[0,1].set_title("feats_B")
+
+    axes[1,0].matshow(reduced_A)
+    axes[1,0].set_title("PCA reduced feats_A")
+    axes[1,1].matshow(reduced_B)
+    axes[1,1].set_title("PCA reduced feats_B")
+
     plt.savefig("features.png", dpi=96, bbox_inches='tight')
 
